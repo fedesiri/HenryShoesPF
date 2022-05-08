d53a5a17a43b357b3f939023eef1c4f653af0f0d
Federico Siri <federicosiri9@gmail.com>
2022-05-08 18:28:56 -0300

    first commit

---
+++ client/src/redux/reducer/index.js
@@ -0,0 +1,19 @@
+
+
+
+
+const intialState = {
+
+};
+
+
+export default function rootReducer (state = intialState, {type, payload}){
+    switch (type) {
+    
+        default: 
+            return {
+            ...state
+            }
+        
+        }
+}
