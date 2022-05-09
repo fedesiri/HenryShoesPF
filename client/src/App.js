faa0e17d175bb682a4f3f523e133811561594a69
fedesiri <94498153+fedesiri@users.noreply.github.com>
2022-05-09 13:15:08 -0300

    removeFiles (#1)

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <h1>HenryShoes</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>HenryShoes</h1>
+        </div>
+    );
 }
 
 export default App;
