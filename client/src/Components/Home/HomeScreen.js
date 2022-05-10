66d57a8f6fb4f402d39f29a3961d95643ccf7a94
Angela Michele Santana Beleño <96745951+malegna@users.noreply.github.com>
2022-05-10 10:43:57 -0500

    creacion componente HomeScreen

---
+++ client/src/Components/Home/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector} from 'react-redux'; 
+import { Link } from 'react-router-dom';
+
+
+export default function HomeScreen (){
+
+
+
+    return (
+        <div>
+            <h1>Aqui se veran todos los productos, con link a su detalle</h1>
+            <Link to = {"cards/id"}></Link>
+        </div>
+
+    )
+}
