724d03d811447e62029cdf8b0a94aa4bd0d0b440
Kimberly Blandon <kimberly.blandong@gmail.com>
2022-05-11 11:08:30 -0300

    create-route-and-controller-getAllBrands

--- api/src/controllers/models.controller.js
+++ api/src/controllers/models.controller.js
@@ -26,6 +26,26 @@ const getAllModels = async (req, res) => {
   }
 };
 
+const getAllBrands = (req, res) => {
+  Models.findAll({
+    attributes: ["brandId"],
+  })
+    .then((brands) => {
+      if (brands.length === 0) {
+        res.status(404).send({ message: "No brands found" });
+      } else {
+        const brandSet = Array.from(
+          new Set(brands.map((brand) => brand.brandId))
+        );
+        res.status(200).send(brandSet);
+      }
+    })
+    .catch((error) => {
+      res.status(500).send({ message: error.message });
+    });
+};
+
 module.exports = {
   getAllModels,
+  getAllBrands
 };
--- api/src/routes/models.js
+++ api/src/routes/models.js
@@ -1,9 +1,10 @@
 var express = require("express");
-const { getAllModels } = require("../controllers/models.controller");
+const { getAllModels, getAllBrands } = require("../controllers/models.controller");
 
 
 var models = express.Router();
 
 models.get("/", getAllModels);
+models.get("/brand", getAllBrands);
 
 module.exports = models;
