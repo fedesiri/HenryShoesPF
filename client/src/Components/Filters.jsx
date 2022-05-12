3927738669e2ffc81e69492a71c28e3b1acd93d1
fedesiri <federicosiri9@gmail.com>
2022-05-12 10:06:23 -0300

    acondicionamiento de filtros y ordenamientos

---
+++ client/src/Components/Filters.jsx
@@ -0,0 +1,35 @@
+export default function Filter({ shoes, handleOrdered, handleFilterGender, handleFilterBrands }) {
+    return (
+        <div>
+            <select onChange={handleOrdered}>
+                <option value="">Seleccione orden</option>
+                <option value="Mayor precio">Mayor Precio</option>
+                <option value="Menor precio">Menor precio</option>
+                <option value="Mas recientes">Mas recientes</option>
+                <option value="Menos recientes">Menos recientes</option>
+            </select>
+
+            <select name="select" onChange={handleFilterGender}>
+                <option value="filterByGender">Gender</option>
+                {shoes?.map(elemento => {
+                    return (
+                        <option value={elemento.gender} key={elemento.id}>
+                            {elemento.gender}
+                        </option>
+                    );
+                })}
+            </select>
+
+            <select name="select" onChange={handleFilterBrands}>
+                <option value="filterByBrands">Brands</option>
+                {shoes?.map(brand => {
+                    return (
+                        <option value={brand.brand} key={brand.id}>
+                            {brand.brand}
+                        </option>
+                    );
+                })}
+            </select>
+        </div>
+    );
+}
