d55aeb4355dab7731569b74d6e906e6c7167949f
Angela Michele Santana Beleño <96745951+malegna@users.noreply.github.com>
2022-05-14 18:37:16 -0500

    ruta delete (#62)

--- api/src/controllers/CreateProduct.js
+++ api/src/controllers/CreateProduct.js
@@ -64,7 +64,44 @@ const modifProduct = (req, res) => {
   })
 }
 
+// const deleteProduct = function(req, res, next){
+//   const { id } = req.body 
+//   let product = Products.findByPk(req.params.id);
+//   let brands = Brands.findByPk(id);
+//   Promise.all([product, brands])
+//   .then(function(values){
+//       let prod = values[0];
+//       let bra = values[1];
+//       prod.removeBrands(bra)
+//   .then(function(removeBrands){
+//       res.status(200).json(removeBrands)
+//   }).catch(function(reason){
+//       res.status(400).json({message:"CATEGORY COULDN'T BE REMOVED", data: reason})
+//   });
+//   });
+// }
+
+const deleteProduct = (req, res) => {
+  const id = req.params.id;
+  Products.destroy({
+    where: {
+      id: id,
+    },
+  })
+    .then((confirmation) => {
+      if (confirmation === 0) {
+        // checking if the id passed its correct
+        return res.send({ data: "Product not found!" }).status(400); // Show proper error in DevTool to the FrontEnd guys.
+      }
+      return res.send("Product Deleted");
+    })
+    .catch((err) => {
+      return res.send({ data: err }).status(400); // Show proper error in DevTool to the FrontEnd guys.
+    });
+};
+
 module.exports = {
   createProduct,
-  modifProduct
+  modifProduct,
+  deleteProduct
 };
--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -2,7 +2,7 @@ var express = require("express");
 const { getAllProducts, getAllBrands, GetShoesByGender } = require("../controllers/products.controller");
 const { getDetails } = require("../controllers/details.controller");
 const { ofertSelect } = require("../controllers/ofertSelect.controller");
-const { createProduct, modifProduct } = require("../controllers/CreateProduct");
+const { createProduct, modifProduct, deleteProduct } = require("../controllers/CreateProduct");
 // const {verifyToken, isAdmin} = require('../middlewares/authJwt.js')
 
 var products = express.Router();
@@ -14,10 +14,12 @@ products.get("/details/:id", getDetails);
 
 products.post("/create", createProduct );
 products.put ("/details/:id", modifProduct)
+products.delete("/details/:id", deleteProduct )
 
 
 products.post("/ofert", ofertSelect)
-products.post("/create", createProduct );
+products.post("/create", createProduct )
+
 
 
 //! Descomentar despues para usar las rutas protegidas con tokens
