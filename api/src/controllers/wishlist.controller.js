8ad9a42e5200e45f3ec4c663840157b96a348b34
Joaquin Navarre <95151285+JoacoNavarre@users.noreply.github.com>
2022-05-18 17:56:54 -0300

    Wishlist back terminada (#91)

---
+++ api/src/controllers/wishlist.controller.js
@@ -0,0 +1,44 @@
+const {Products, User} = require("../db.js");
+
+const AddProduct = async(req, res) => {
+const {productId, userName} = req.body
+try{
+ const addedProduct = await Products.findOne({
+     where:{
+         id: productId
+     }
+ });
+ const userWishlist = await User.findOne({
+     where:{
+         username: userName
+     }
+ });
+ userWishlist.addProduct(addedProduct);
+ res.send(userWishlist)
+}catch(err){
+    res.status(500).send(err)
+}
+};
+
+const getWishlist = async (req, res) => {
+const {userName} = req.body
+try{
+const wishlist = await User.findOne({
+    where:{
+        username: userName
+    },
+    include: {
+        model: Products,
+        attributes: ["model"],
+      },
+})
+res.send(wishlist)
+}catch(err){
+res.status(404).send("la wichlist está vacía")
+}
+};
+
+module.exports = {
+    AddProduct,
+    getWishlist
+  };
