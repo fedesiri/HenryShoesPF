8c7b9fa1f08dd16e8b6a3765391a9f8daaebffe7
Lucas Manuel <49735490+Lucas-aquiles@users.noreply.github.com>
2022-05-19 17:09:59 -0300

    redux_persist (#99)
    se_instala y configura , esta andandoooooooo

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -3,8 +3,25 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "../reducer/index";
 
-const store = createStore(
-  rootReducer,
+import { persistStore, persistReducer } from 'redux-persist'
+
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ["shoppingCart"]
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer,
   composeWithDevTools(applyMiddleware(thunk))
 ); // rootReducer is the function that returns the state
-export default store;
+
+export const persistor = persistStore(store)
+
+
+
+
