53b739c69cdb866cb6d3386c1aaaf7774fc8b8b1
fedesiri <federicosiri9@gmail.com>
2022-05-26 18:16:29 -0300

    creacion de rutas user en back

--- api/src/controllers/user.controller.js
+++ api/src/controllers/user.controller.js
@@ -1,4 +1,59 @@
 import User from "../models/User.js";
 
 
+export const getAllUsers = async (req, res) => {
+    try{
+        const allUser = await User.findAll()
+        res.send(allUser)
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+}
+
+export const getUserById = async (req, res) => {
+    try{
+        const oneUser = await User.findByPk(req.params.id)
+        res.send(oneUser)
+    } catch (error){
+        res.status(500).send({ message: error.message });
+    }
+}
+
+export const editUser = async (req, res) => {
+    const { name, email, password, lastname } = req.body 
+    try{
+        await User.update(
+            {
+              name: name,
+              email: email,
+              password: password,
+              lastname: lastname,            
+            },
+            {
+              where: {
+                id: req.params.id,
+              },
+            }
+          );
+          res.send({message: 'User has been update succesfuly!'})
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+}
+  
+  
+  export const deleteUser = (req, res) => {
+      try{
+          User.destroy({
+            where: {
+              id: req.params.id,
+            },
+          })
+          return res.send("User has been deleted successfully");
+
+      } catch (error){
+        return res.send({ message: err }).status(400);
+      }      
+  };
+
 
