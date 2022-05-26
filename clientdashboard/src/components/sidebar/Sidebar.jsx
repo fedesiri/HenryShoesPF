e930302a0b8999352467a2c5ffed2f4a47bf1037
Kimberly Blandon <94016613+Kimblag@users.noreply.github.com>
2022-05-26 11:31:50 -0300

    arreglados detalles edit product (#135)

--- clientdashboard/src/components/sidebar/Sidebar.jsx
+++ clientdashboard/src/components/sidebar/Sidebar.jsx
@@ -15,43 +15,43 @@ export default function Sidebar() {
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Dashboard</h3>
           <ul className="sidebarList">
-            <Link to="/" className="link">
+            <Link style={{textDecoration: "none"}} to="/" className="link">
             <li className="sidebarListItem active">
               <LineStyle className="sidebarIcon" />
               Home
             </li>
             </Link>
-            <Link to="/users" className="link">
+            <Link style={{textDecoration: "none"}} to="/users" className="link">
               <li className="sidebarListItem">
                 <PermIdentity className="sidebarIcon" />
                 Users
               </li>
             </Link>
-            <Link to="/products" className="link">
+            <Link style={{textDecoration: "none"}} to="/products" className="link">
               <li className="sidebarListItem">
                 <Storefront className="sidebarIcon" />
                 Products
               </li>
             </Link>
-            <Link to="/create-products" className="link">
+            <Link style={{textDecoration: "none"}} to="/create-products" className="link">
               <li className="sidebarListItem">
                 <CreateIcon className="sidebarIcon" />
                 Create Products
               </li>
             </Link>
-            <Link to="/create-oferta" className="link">
+            <Link style={{textDecoration: "none"}} to="/create-oferta" className="link">
               <li className="sidebarListItem">
                 <CreateIcon className="sidebarIcon" />
                 Create Ofert and BetSellers
               </li>
             </Link>
-            <Link to="/edit-product" className="link">
+            {/* <Link to="/edit-product" className="link">
               <li className="sidebarListItem">
                 <CreateIcon className="sidebarIcon" />
                 Edit Product
               </li>
-            </Link>
-            <Link to="/create-category" className="link">
+            </Link> */}
+            <Link style={{textDecoration: "none"}} to="/create-category" className="link">
               <li className="sidebarListItem">
                 <CreateIcon className="sidebarIcon" />
                 Create Category
