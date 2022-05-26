dd4f6218b31ce63217b68d468ac3426dc808eeb6
fedesiri <94498153+fedesiri@users.noreply.github.com>
2022-05-26 18:17:24 -0300

    creacion de rutas user en back (#143)

--- api/src/routes/user.routes.js
+++ api/src/routes/user.routes.js
@@ -1,4 +1,5 @@
 import {Router} from "express";
+import { editUser, getAllUsers, getUserById, deleteUser } from "../controllers/user.controller.js";
 import { isUserAuthenticated } from "../middlewares/auth.js";
 
 const router = Router();
@@ -7,6 +8,9 @@ router.get('/auth', isUserAuthenticated, (req, res) => {
     res.send(req.user);
 })
 
-
+router.get('/', getAllUsers)
+router.get('/:id', getUserById) 
+router.put('/edit/:id', editUser)
+router.delete('/delete/:id', deleteUser)
 
 export default router;
