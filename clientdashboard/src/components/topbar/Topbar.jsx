77bf49132d412b710a7d0603e97c96dcb011d8e6
fedesiri <94498153+fedesiri@users.noreply.github.com>
2022-05-27 13:00:54 -0300

    arreglitos en user/dashboard (#151)

--- clientdashboard/src/components/topbar/Topbar.jsx
+++ clientdashboard/src/components/topbar/Topbar.jsx
@@ -10,7 +10,7 @@ export default function Topbar() {
           <span className="logo">Henry Shoes</span>
         </div>
         <div className="topRight">
-          <div className="topbarIconContainer">
+          {/* <div className="topbarIconContainer">
             <NotificationsNone />
             <span className="topIconBadge">2</span>
           </div>
@@ -20,7 +20,7 @@ export default function Topbar() {
           </div>
           <div className="topbarIconContainer">
             <Settings />
-          </div>
+          </div> */}
           {/* <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" /> */}
         </div>
       </div>
