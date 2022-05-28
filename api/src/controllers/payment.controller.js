1476e316cc4e7589c0676ab8931cd18383aa0392
Kimberly Blandon <kimberly.blandong@gmail.com>
2022-05-27 21:55:22 -0300

    creado handle de paypal

--- api/src/controllers/payment.controller.js
+++ api/src/controllers/payment.controller.js
@@ -3,47 +3,82 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const createPayment = async (req, res) => {
-  const order = {
-    intent: "CAPTURE",
-    purchase_units: [
-      // array de las unidades de compra
-      {
-        amount: {
-          currency_code: "USD",
-          value: "100.00",
+    // console.log(req.body)
+  try {
+    const order = {
+      intent: "CAPTURE",
+      purchase_units: [
+        // array de las unidades de compra
+        {
+          amount: {
+            currency_code: "USD",
+            value: req.body.total,
+          },
+          description: "Compra de productos",
         },
-        description: "Compra de productos",
+      ],
+      application_context: {
+        brand_name: "HenryShoes",
+        landing_page: "LOGIN",
+        user_action: "PAY_NOW",
+        return_url: "http://localhost:3001/payment/capture-order", //Si pagas con paypal, esta es la url de la pagina de retorno
+        cancel_url: "http://localhost:3001/payment/cancel-order", //si cancela la compra, esta es la url de la pagina de retorno
       },
-    ],
-    application_context: {
-      brand_name: "HenryShoes",
-      landing_page: "LOGIN",
-      user_action: "PAY_NOW",
-      return_url: "http://localhost:3000/payment/capture-order", //Si pagas con paypal, esta es la url de la pagina de retorno
-      cancel_url: "http://localhost:3000/payment/cancel-order", //si cancela la compra, esta es la url de la pagina de retorno
-    },
-  };
+    };
 
-  //Peticion a la ruta post de paypal
-  const response = await axios.post(
-    `${process.env.PAYPAL_API}/v2/checkout/orders`,
-    order,
-    {
-      auth: {
-        username: process.env.PAYPAL_API_CLIENT,
-        password: process.env.PAYPAL_API_SECRET,
-      },
-    }
-  );
-  console.log(response.data);
+    //para colocar valores dentro de una URL. No puedo directamente porque es como un formulario
+    const params = new URLSearchParams();
+    params.append("grant_type", "client_credentials");
 
-  res.send("creating order");
+    const {
+      data: { access_token },
+    } = await axios.post(
+      `https://api-m.sandbox.paypal.com/v1/oauth2/token`,
+      params,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        auth: {
+          username: process.env.PAYPAL_API_CLIENT,
+          password: process.env.PAYPAL_API_SECRET,
+        },
+      }
+    );
+
+    //Peticion a la ruta post de paypal
+    const response = await axios.post(
+      `${process.env.PAYPAL_API}/v2/checkout/orders`,
+      order,
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+    // console.log(access_token);
+    // console.log(response.data);
+
+    res.json(response.data);
+  } catch (error) {
+    return res.status(500).send({ message: "Something went wrong" });
+  }
 };
 
 export const capturePayment = async (req, res) => {
-  res.send("capture order");
+    const { token } = req.query;
+    // console.log(token);
+    const response = await axios.post(`${process.env.PAYPAL_API}/v2/checkout/orders/${token}/capture`, {}, {
+        auth: {
+            username: process.env.PAYPAL_API_CLIENT,
+            password: process.env.PAYPAL_API_SECRET,
+        }
+    })
+
+    // console.log(response.data);
+  res.send("capture order"); //! redireccionar a la pagina de gracias por comprar o perfil usuario/ordenes
 };
 
 export const cancelPayment = async (req, res) => {
-  res.send("cancel order");
+  res.send("cancel order"); //! redireccionar a la pagina del carrito
 };
--- api/src/routes/payment.routes.js
+++ api/src/routes/payment.routes.js
@@ -4,7 +4,7 @@ import { cancelPayment, capturePayment, createPayment } from '../controllers/pay
 const router = Router();
 
 
-router.get('/create-order', createPayment)
+router.post('/create-order', createPayment)
 
 router.get('/capture-order', capturePayment)
 
