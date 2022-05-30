b7df2372eb31cd0ac2d7dd286169e3bef47905e8
Joaquin Navarre <95151285+JoacoNavarre@users.noreply.github.com>
2022-05-29 23:51:36 -0300

    Remove from cart (#184)

--- api/src/routes/shoppingCart.routes.js
+++ api/src/routes/shoppingCart.routes.js
@@ -1,10 +1,11 @@
 import {Router} from 'express'
-import { getShoppingCart, getShoppingHistory } from '../controllers/shoppingCart.controller.js';
+import { getShoppingCart, getShoppingHistory, removeFromCart } from '../controllers/shoppingCart.controller.js';
 
 const router = Router()
 
 router.get("/", getShoppingCart);
 router.get("/History", getShoppingHistory);
+router.post("/remove", removeFromCart)
 
 
 
