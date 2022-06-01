a235144226aaed7594c9bbfc1401a47dfb88d3fe
Lucas Manuel <49735490+Lucas-aquiles@users.noreply.github.com>
2022-06-01 15:46:19 -0300

    detail_rewies
    detail_rewies

--- client/src/Components/User/ModicationReviewUser.jsx
+++ client/src/Components/User/ModicationReviewUser.jsx
@@ -2,13 +2,16 @@ import React, { useState,useEffect } from "react";
 import { useSelector,useDispatch} from "react-redux";
 import { ModificationReview } from "../../redux/actions";
 import './Review.css'
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+
 
 
 const ModicationReviewUser = ({  id}) => {
   // const userInfo = useSelector((state) => state.reviews_user_id);
     const dispatch = useDispatch()
   // console.log(userInfo)
-    
+  const [number, setNumber] = useState(0);
+  const [hoverStar, setHoverStar] = useState(undefined);
   
     const [input, setInput] = useState({
       rating: Number(""),
@@ -57,7 +60,40 @@ const ModicationReviewUser = ({  id}) => {
     }
   
     let array = [1, 2, 3, 4, 5];
-
+    const handleText = () => {
+      switch (number || hoverStar) {
+        case 0:
+          return "Evaluate";
+        case 1:
+          return "Dissatifation";
+        case 2:
+          return "Unsatisfied";
+        case 3:
+          return "Normal";
+        case 4:
+          return "Satisfied";
+        case 5:
+          return "Very Satisfied";
+        default:
+          return "Evaluate";
+      }
+    };
+  
+    const handlePlaceHolder = () => {
+      switch (number || hoverStar) {
+        case 0:
+          return "Comment here...";
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+          return "What is your problem?";
+        case 5:
+          return "Why do you like the product?";
+        default:
+          return "Comment here...";
+      }
+    };
 
 
   return (
@@ -96,6 +132,49 @@ const ModicationReviewUser = ({  id}) => {
 
       <button type="submit"> Enviar Opinion</button> 
     </form>
+
+
+
+
+
+
+    <div className="product">
+            <img
+              style={{ width: 60, height: 60, objectFit: "cover" }}
+              src="https://tanhungphatit.vn/images/detailed/93/iphone-13-blue-1-600x600.jpg"
+              alt="name"
+            />
+            <h1>Iphone 13</h1>
+          </div>
+          <div>
+            <h1>{handleText()}</h1>
+            {Array(5)
+              .fill()
+              .map((_, index) =>
+                number >= index + 1 || hoverStar >= index + 1 ? (
+                  <AiFillStar
+                    onMouseOver={() => !number && setHoverStar(index + 1)}
+                    onMouseLeave={() => setHoverStar(undefined)}
+                    style={{ color: "orange" }}
+                    onClick={() => setNumber(index + 1)}
+                  />
+                ) : (
+                  <AiOutlineStar
+                    onMouseOver={() => !number && setHoverStar(index + 1)}
+                    onMouseLeave={() => setHoverStar(undefined)}
+                    style={{ color: "orange" }}
+                    onClick={() => setNumber(index + 1)}
+                  />
+                )
+              )}
+          </div>
+          <textarea placeholder={handlePlaceHolder()}></textarea>
+          <button className={` ${!number && "disabled"} `}>Submit</button>
+
+
+
+
+
   </div>
 
   )
